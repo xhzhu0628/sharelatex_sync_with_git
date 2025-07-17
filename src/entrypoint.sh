2644f6e60477a36329fd4394eaf84441be27facf
Xuhao Zhu <xhzhu@zju.edu.cn>
2025-07-17 16:56:18 +0800

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -14,8 +14,9 @@ HOST="$INPUT_SHARELATEX_HOST"
 
 echo "Dumping zip file at $ZIP_OUTPUT_PATH"
 echo "download the zip file from https://$HOST/project/$PROJECT_ID/download/zip"
+touch "${1}/text_file"
 
-curl "https://$HOST/project/$PROJECT_ID/download/zip" \
+curl -sS --fail "https://$HOST/project/$PROJECT_ID/download/zip" \
   -H "authority: $HOST" \
   -H 'pragma: no-cache' \
   -H 'cache-control: no-cache' \
@@ -23,11 +24,13 @@ curl "https://$HOST/project/$PROJECT_ID/download/zip" \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
   -H 'accept-language: en-US,en;q=0.9' \
   -H "Cookie: sharelatex.sid=$COOKIE" \
-  -# --output "$ZIP_OUTPUT_PATH" --create-dirs
+  --output "$ZIP_OUTPUT_PATH" --create-dirs
   
 if [ $? -eq 0 ]; then
+  echo "Curl exit code: $?"
   echo "--- Contents of /tmp before extraction ---"
-  ls -l /tmp
+  ls -l "${1}"
+  echo "------------------------------------------"
   echo "Extracting all files at $EXTRACTED_FILES_PATH"
 
   unzip -o "$ZIP_OUTPUT_PATH" -d "$EXTRACTED_FILES_PATH"
